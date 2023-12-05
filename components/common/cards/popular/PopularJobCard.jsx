547a41ac950ca07dca0be1c1af07f4ebf201f353
dlshan-baryon-dev-at-619377243340 <dperera@baryon.se>
2023-12-05 20:24:50 +0530

    initiate file structure and basic UIs

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+import styles from "./popularjobcard.style";
+// import { checkImageURL } from "../../../../utils";
+
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+  return (
+    <Text> Popular Job</Text>
+  );
+};
+
+export default PopularJobCard;
