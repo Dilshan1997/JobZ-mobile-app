b9196a1fd5ae4ba875ec66befab120119a7f1806
dlshan-baryon-dev-at-619377243340 <dperera@baryon.se>
2023-12-06 09:21:49 +0530

    update the usefetch hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import { axios } from "axios";
-import {RAPID_API_KEY} from "@env";
 
-const rapidApiKey = RAPID_API_KEY;
+// import { RAPID_API_KEY } from '@env';
+
+// const rapidApiKey = RAPID_API_KEY;
 
 const useFetch=(endpoint, query)=>{
     const [data, setData] = useState([]);
@@ -14,7 +15,7 @@ const useFetch=(endpoint, query)=>{
       method: 'GET',
       url: `https://jsearch.p.rapidapi.com/${endpoint}`,
       headers: {
-        'X-RapidAPI-Key': rapidApiKey,
+        'X-RapidAPI-Key': '69cc20368dmsh8f68a3e8b817f03p119946jsnce2f011c2412',
         'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
       },
       params: {...query},
